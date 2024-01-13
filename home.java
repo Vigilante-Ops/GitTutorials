bcbde52dd4be7f649f6275e201acb3d1cb200bf6
Vigilante-Ops <amandixit99@outlook.com>
2024-01-13 10:12:56 +0530

    home.java login.java

---
+++ home.java
@@ -0,0 +1,7 @@
+package src.java;
+public class home
+{
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ login.java
@@ -0,0 +1,9 @@
+package src.java;
+
+public class login 
+{
+    public static void main(String[] args) {
+        System.out.println("login");
+    }
+    
+}
